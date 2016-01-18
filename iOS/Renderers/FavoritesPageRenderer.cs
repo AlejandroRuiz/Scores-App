8164fca9b538930fcdf2a0f9373d4b446f97653b
Alejandro Ruiz Varela <elgoberlivel@gmail.com>
2016-01-18 09:37:14 -0600

    iOS 3D Touch and Spotlight Search Features Added

---
+++ iOS/Renderers/FavoritesPageRenderer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Xamarin.Forms.Platform.iOS;
+using Foundation;
+using ScoresApp.iOS.Renderers;
+using ScoresApp.Pages;
+using Xamarin.Forms;
+using CoreSpotlight;
+
+[assembly:ExportRenderer(typeof(WelcomePage), typeof(FavoritesPageRenderer))]
+namespace ScoresApp.iOS.Renderers
+{
+	public class FavoritesPageRenderer:PageRenderer
+	{
+		public FavoritesPageRenderer ()
+		{
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+			UserActivity = CreateActivity ();
+		}
+
+		const string activityName = "com.alejandroruiz.scores-app.favorites";
+
+		NSUserActivity CreateActivity()
+		{
+			var activity = new NSUserActivity(activityName);
+			activity.EligibleForSearch = true;
+			activity.EligibleForPublicIndexing = true;
+			activity.EligibleForHandoff = false;
+
+			activity.Title = "Favorites";
+			activity.AddUserInfoEntries(NSDictionary.FromObjectAndKey(new NSString("Favorites"), new NSString("Name")));
+
+			//var keywords = new string[] {  };
+			activity.Keywords = new NSSet<NSString>(new NSString("Favorites"), new NSString("Soccer"), new NSString("Favorites"));
+			activity.ContentAttributeSet = new CSSearchableItemAttributeSet("Open favorites");
+
+			return activity;
+		}
+	}
+}
+
